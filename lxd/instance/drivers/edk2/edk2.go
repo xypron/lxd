ce98e3218273ade45b0aaebd1623b4ea087f3c31
Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
2025-06-28 06:17:48 +0200

    lxd/instance: add riscv64 QEMU definitions
    Add missing definitions to run virtual RISC-V machines.
    This is enough to build an LXD snap that support VMs.
    
    For RISC-V there is no trusted party to sign shim for all operating systems
    like Microsoft does for x86 and ARM. As LXD defaults to secure boot add
    fake entries for the firmware.
    
    Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>

--- lxd/instance/drivers/edk2/edk2.go
+++ lxd/instance/drivers/edk2/edk2.go
@@ -148,6 +148,20 @@ var architectureInstallations = map[int][]Installation{
 			},
 		},
 	}},
+	osarch.ARCH_64BIT_RISCV_LITTLE_ENDIAN: {{
+		Paths: GetenvEdk2Paths("/usr/share/qemu-efi-riscv64"),
+		Usage: map[FirmwareUsage][]FirmwarePair{
+			GENERIC: {
+				{Code: "RISCV_VIRT_CODE.fd", Vars: "RISCV_VIRT_VARS.fd"},
+				{Code: "OVMF_CODE.4MB.fd", Vars: "OVMF_VARS.4MB.fd"},
+			},
+			SECUREBOOT: {
+				// Secure boot is not supported, yet. Fake it.
+				{Code: "RISCV_VIRT_CODE.fd", Vars: "RISCV_VIRT_VARS.fd"},
+				{Code: "OVMF_CODE.4MB.fd", Vars: "OVMF_VARS.4MB.fd"},
+			},
+		},
+	}},
 }
 
 // GetAchitectureFirmwareVarsCandidates returns a unique list of candidate vars names for hostArch for all usages.
